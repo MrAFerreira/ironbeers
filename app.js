508fddf0a30f5719c1d9144435f1d40ca967572f
MrAFerreira <andre.f.v.ferreira@gmail.com>
2022-08-29 18:57:48 +0100

    removes auth from routes

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ const index = require('./routes/index.routes');
 app.use('/', index);
 
 const auth = require('./routes/auth.routes');
-app.use('/auth', auth);
+app.use('/', auth);
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require('./error-handling')(app);
